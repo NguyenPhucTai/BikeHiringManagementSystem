1e58974e0a35d53ac390e1f604b328a5fc8db281
Nguyễn Phúc Tài <62977249+NguyenPhucTai@users.noreply.github.com>
2022-09-11 14:07:16 +0700

    Update Bike

--- Back-end/src/main/java/com/BikeHiringManagement/entity/Bike.java
+++ Back-end/src/main/java/com/BikeHiringManagement/entity/Bike.java
@@ -11,6 +11,9 @@ public class Bike extends BaseEntity{
     @Column(name = "name", length = 100)
     private String name;
 
+    @Column(name = "bike_manual_id", nullable = false)
+    private String bikeManualId;
+
     @Column(name = "bike_no", length = 50, nullable = false)
     private String bikeNo;
 
