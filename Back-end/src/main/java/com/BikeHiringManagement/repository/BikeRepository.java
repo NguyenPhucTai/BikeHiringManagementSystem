d8e7f51de2a65def37155cb3479098e4f53a29d7
Nguyễn Phúc Tài <62977249+NguyenPhucTai@users.noreply.github.com>
2023-03-23 23:02:19 +0700

    BE: Delete color and manufacturer not allow when bike using this

--- Back-end/src/main/java/com/BikeHiringManagement/repository/BikeRepository.java
+++ Back-end/src/main/java/com/BikeHiringManagement/repository/BikeRepository.java
@@ -10,21 +10,20 @@ import java.util.List;
 public interface BikeRepository extends JpaRepository<Bike, Long>, JpaSpecificationExecutor<Bike> {
     boolean existsByIdAndIsDeleted(Long id, Boolean check);
     boolean existsByNameAndIsDeleted(String name, Boolean check);
-
-    boolean existsByIdAndBikeManualIdAndIsDeleted(Long bikeId, String bikeManualId, Boolean check);
     boolean existsByBikeNoAndName(String bikeNo, String name);
-
-    boolean existsByIdAndStatusAndIsDeleted(Long id, String status, Boolean check);
     boolean existsByBikeManualIdAndIsDeleted(String manualId, Boolean check);
 
     Bike findBikeById(Long id);
     Bike findBikeByBikeManualId(String manualId);
 
-    Integer countBikesByBikeCategoryIdAndIsDeleted(Long categoryId, Boolean check);
-    Integer countBikesByIsDeleted(Boolean check);
-
-    @Query("SELECT b, ct.name FROM Bike b inner join BikeCategory ct on b.bikeCategoryId = ct.id")
-    List<Bike> findBikeByIdCustom();
-
     List<Bike> findAllByIdInAndIsDeleted(List<Long> listBikeId, Boolean check);
+    List<Bike> findAllByBikeColorIdAndIsDeleted(Long colorId, Boolean check);
+    List<Bike> findAllByBikeManufacturerIdAndIsDeleted(Long manufacturerId, Boolean check);
+
+//    boolean existsByIdAndBikeManualIdAndIsDeleted(Long bikeId, String bikeManualId, Boolean check);
+//    boolean existsByIdAndStatusAndIsDeleted(Long id, String status, Boolean check);
+//    Integer countBikesByBikeCategoryIdAndIsDeleted(Long categoryId, Boolean check);
+//    Integer countBikesByIsDeleted(Boolean check);
+//    @Query("SELECT b, ct.name FROM Bike b inner join BikeCategory ct on b.bikeCategoryId = ct.id")
+//    List<Bike> findBikeByIdCustom();
 }
